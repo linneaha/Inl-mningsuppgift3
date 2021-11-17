529d945fa547504223c8f7f7220cd8ab95b24e48
Linnea Hensfelt Andersson <linneahensfelta@gmail.com>
2021-11-17 13:11:58 +0100

    Quiz questions done

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
  
  // dark/light-mode
  document.querySelector("#switchMode").addEventListener("click", () => {
-
+    let body = document.body;
+    if (body.style.backgroundColor === "black") {
+    body.style.backgroundColor = "bisque";
+    } else {
+    body.style.backgroundColor = "black";
+    }
  })
+
+ // 
